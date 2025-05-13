ac43dbeb59652706672426daa2dfde241365df00
fishbread <123308170+fish-bread@users.noreply.github.com>
2025-05-13 20:09:08 +0800

    Delete demo directory

--- demo/demo/function/shoppingCar.js
+++
@@ -1,4 +0,0 @@
-import { ref } from 'vue'
-export const goods = ref([
-    { store_name: '111', goods: [{goods_name: '22', goods_img: '/static/logo.png', goods_num: '1' ,goods_sold: '111', goods_price: '3' }]
-}])
